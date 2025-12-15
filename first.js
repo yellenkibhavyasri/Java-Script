09e1722657e71c848fb6be66decdd6788d8ca67d
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2025-12-15 21:13:52 +0530

    Java Script Day-1

---
+++ first.js
@@ -0,0 +1,33 @@
+/*let age=10;
+age=89;
+console.log(age);
+const PI=3.14;
+console.log(PI);*/
+/*{
+    let a=5;
+    console.log(a);
+}*/
+/*age=25;
+console.log(age);
+console.log(typeof age);*/
+/*const student={
+    fullName: "Shiva",
+    age:20,
+    cgp:9.6,
+};
+console.log(student.age);*/
+//exercise to store the info of the product
+/*const product={
+    name:"Pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);*/
+// instagram profile
+const profile={
+    username:"Bhavya",
+    followers:90,
+    isFollow: false,
+    following:123,
+};
