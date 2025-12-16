f43453cff794690cc381d1027eb2f4e3d5269b84
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2025-12-16 19:00:58 +0530

    Operators and Conditional Statements

---
+++ second.js
@@ -0,0 +1,56 @@
+//Arithmetic Operators
+/*let a=9;
+let b=8;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);*/
+// unary Operators
+/*let a=4;
+let b=8;
+a++;
+b--;
+console.log(a);
+console.log(b);8*/
+//Assignment operators
+/*let a=7;
+let b=6;
+a+=4;
+b*=9;
+console.log(a);
+console.log(b);*/
+//comparison operators
+/*let a=9;
+let b=7;
+console.log(a>=b);
+console.log(a!=b);
+console.log(a===b);*/
+//logical operators
+/*let a=6;
+let b=8;
+let cond1=a>b;
+let cond2=a===6;
+console.log(cond1&&cond2);
+console.log(cond1||cond2);
+console.log(!(a<b));*/
+//Conditional Statements
+/*let age=25;
+if(age>18){
+    console.log("You can vote");
+}*/
+//ternary operators
+/*let age=25;
+let res=age>=18 ? "adult" : "not adult";
+console.log(res);*/
+//practice q1
+alert("hello!!");
+prompt("helloo");
+let name=prompt("helloo");
+console.log(name);
+//question: get user input a number using prompt("") . Check if the number is a muktiple of 5 or not.
+let num=prompt("Enter a number:");
+if(num%5===0){
+    console.log("num is multiple of 5");
+}else{
+    console.log("num is not multiple of 5");
+}
