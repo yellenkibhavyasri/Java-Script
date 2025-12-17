a513e2b1209802bb658991206ac9fea2e29cbae3
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2025-12-17 22:15:24 +0530

    loops and strings

---
+++ third.js
@@ -0,0 +1,53 @@
+/*for(let count=1;count<=5;count++){
+    console.log("Hellloooo");
+}
+console.log("loop has ended");*/
+//calculate sum of 1 to 5
+/*let sum=0;
+for(let i=0;i<=5;i++){
+    sum+=i;
+}
+console.log(sum);*/
+/*let i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+}*/
+/*let i=20;
+do{
+    console.log("helloo");
+    i++;
+}
+while(i<=10);*/
+//for-of loop
+/*let str="JavaScript";
+for(let i of str){// iterator-> characters
+    console.log(i);
+}*/
+//for-in-loop
+/*let student={
+    name:"Shiv",
+    age:20,
+    cgpa:8.5,
+    isPass: true
+};
+for(let i in student){
+console.log(i,student[i]);
+}*/
+//practice print all odd numbers
+/*for(let i=0;i<=100;i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}*/
+//practice 2 create a game where you start with any random game numebr. Ask the user to keep guessing the game number until the user enters correct value
+ /*let gamenumber=50;
+ let usernum=prompt("guess the number");
+ console.log(usernum);
+ while(usernum!=gamenumber){
+usernum=prompt("u have entered wrong number");
+ }
+console.log("you have entered correct number");*/
+
+//strings
+let str="Namaste";
