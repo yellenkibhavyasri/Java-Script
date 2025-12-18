422afa71fe76f4a0a1d63d5baa663c770528d479
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2025-12-18 19:23:44 +0530

    Arrays

---
+++ fourth.js
@@ -0,0 +1,24 @@
+/*let marks=[97,67,54,78,90];
+console.log(marks);
+console.log(marks.length);
+for(let i=0;i<=marks.length;i++){
+    console.log(marks[i]);
+}
+let city=["Hyd","Mumbai","Chennai","Surat"];
+for(let j of city){
+    console.log(j.toUpperCase());
+}*/
+// practice for a given array find the average marks of the class
+/*let marks=[85,97,44,37,76,60];
+let sum=0;
+for(let i=0;i<marks.length;i++){
+    sum+=marks[i];
+}
+let avg=sum/marks.length;
+console.log(avg);*/
+// Array Methods
+let veggies=["Onion","carrot","Mirchi","Tomato"];
+console.log(veggies.push("brinjal"));
+console.log(veggies);
+console.log(veggies.pop());
+console.log(veggies);
