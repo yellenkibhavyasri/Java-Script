33766f69aac14b45888c92ac434edaad2763b642
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2025-12-21 14:01:02 +0530

    Document Object Model

---
+++ sixth.js
@@ -0,0 +1,14 @@
+//console.dir(document.body);
+//accessing elements
+/*let heading=document.getElementById("heading");
+console.dir(heading);
+let cls=document.getElementsByClassName("class");
+console.dir(cls);
+console.log(cls);*/
+
+/*let firstele=document.querySelector("p");
+console.dir(firstele);
+let allEl=document.querySelectorAll("p");
+console.dir(allEl);*/
+let div=document.querySelector("div");
+console.dir(div);
