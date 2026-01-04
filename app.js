5e770cb1c0bd5f5f0a8ec64ad5a63f801b3e3922
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2026-01-04 10:07:37 +0530

    Tic Tac Toe

---
+++ app.js
@@ -0,0 +1,42 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let turnO=true; //player0, playerx
+const winpattern=[
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7 ,8],
+];
+boxes.forEach((box) =>{
+    box.addEventListener("click",() =>{
+       console.log("box was clicked");
+       if(turnO){
+        box.innerText="O";
+        turnO=false;
+       }else{
+        box.innerText="X";
+        turnO=true;
+       }
+       box.disabled=true;
+       checkwinner();
+    });
+}
+);
+const checkwinner = () =>{
+    for(pattern of winpattern){
+    
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+        if(pos1val!=" " && pos2val!=" " && pos3val!=" "){
+            if(pos1val=== pos2val && pos2val=== pos3val){
+                console.log("winner");
+            }
+        }
+    }
+}
